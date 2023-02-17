873f55059efeb3dbd65d5f80278189848fe96b4e
Daniel Diaz <710147@student.dusd.net>
2023-02-17 11:43:16 -0800

    added the bonuss.
    added the bonuss.

--- PlayerControlDiazP3/Assets/Scripts/PlayerController.cs
+++ PlayerControlDiazP3/Assets/Scripts/PlayerController.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Private Variables
-    private float speed = 20.0f;
-    private float turnSpeed = 70.0f;
+    private float speed = 30.0f;
+    private float turnSpeed = 90.0f;
     private float horizontalInput;
     private float forwardInput;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
 
     // Start is called before the first frame update
     void Start()
     {
-         
+
     }
 
     // Update is called once per frame
@@ -26,6 +29,12 @@ public class PlayerController : MonoBehaviour
         // We'll move the vehicle forward 
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         // We turn the vehicle
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
